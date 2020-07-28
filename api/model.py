c297eeb1031776130b4ef1682234a0bc9e9144f6
BigBlackWolf <stepan.rasputny@gmail.com>
2020-07-28 17:29:33 +0300

    Changes in db schema

--- api/model.py
+++ api/model.py
@@ -13,6 +13,11 @@ product_category = db.Table(
     db.Column('category_id', db.Integer, db.ForeignKey('categories.id'), primary_key=True)
 )
 
+product_flower = db.Table(
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('flower_id', db.Integer, db.ForeignKey('flower.id'), primary_key=True)
+)
+
 
 class Role(enum.Enum):
     ADMIN = 0
@@ -37,9 +42,19 @@ class Product(db.Model):
     price = db.Column(db.DECIMAL)
     title = db.Column(db.String(255))
     description = db.Computed(db.Text, default="")
-    ingredients = db.Column(db.Integer, )
+    ingredients = db.relationship('Flower', secondary=product_flower, lazy='subquery',
+                                  backref=db.backref('product', lazy=True))
     categories = db.relationship('Category', secondary=product_category, lazy='subquery',
                                  backref=db.backref('products', lazy=True))
+
+
+class UserAddress(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    city = db.Column(db.String(100))
+    street = db.Column(db.String(200))
+    house = db.Column(db.Integer)
+    flat = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
 class Order(db.Model):
@@ -47,11 +62,11 @@ class Order(db.Model):
     order_price = db.Column(db.DECIMAL)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
     payment_method = db.Column(db.Enum(PaymentMethod))
-    comments = db.Column(db.Text)
+    comment = db.Column(db.Text)
     user_name = db.Column(db.String(255))
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
     phone = db.Column(db.String(20))
     address = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
 
 
 class User(db.Model):
@@ -62,12 +77,11 @@ class User(db.Model):
     username = db.Column(db.String(255), nullable=False)
     gender = db.Column(db.Enum(Gender))
     phone = db.Column(db.String(255), unique=True, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
     role = db.Column(db.Enum(Role), nullable=False)
+    addresses = db.relationship('UserAddresses', back_populates='user')
     orders = db.relation('Order', backref='user', lazy=True)
     # role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     # role = db.Column('Role', back_populates='users')
@@ -98,6 +112,7 @@ class News(db.Model):
     sub_title = db.Column(db.String(255))
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     body = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
 
 
 class Category(db.Model):
@@ -105,6 +120,17 @@ class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+
+
+class Flower(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+
+
+class Metadata(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    call_center_phone = db.Column(db.String(20))
+    locations = db.Column(db.ARRAY(db.String))
 
 
 # class Role(db.Model):
